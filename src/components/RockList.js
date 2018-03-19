74cdee5f643d98a0b0a161b66a4fb020cdb4b3f5
Amalia Claire Fergin <acfergin@msn.com>
2018-03-19 14:14:23 -0700

    create input

--- src/components/RockList.js
+++ src/components/RockList.js
@@ -3,6 +3,7 @@ import Rock from './Rock'
 
 class RockList extends Component {
   state = {
+    message: 'My class is no fun and uncreative today',
       rocks: [
            {
             name: 'stoner',
@@ -24,7 +25,9 @@ class RockList extends Component {
     let rockElements = this.state.rocks.map((rock, i) => <Rock key={i} {...rock} />)
     return (
       <div>
+      <h1>Important Message! {this.state.message} </h1>
         <h3> list of rocks for you to mingle with </h3>
+        <input onChange={(e) => this.setState({message: e.target.value})} />
         {rockElements}
       </div>
     )
